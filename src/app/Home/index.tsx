39e58cfc36eea197c57a774e4061df76c990d9a0
tarcisiowes <tarcisiowesley@gmail.com>
2026-02-24 13:47:52 +0900

    chore: code style config

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,12 @@
-import {StatusBar} from 'expo-status-bar';
-import {Text, View} from 'react-native';
-import {styles} from "./styles";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles } from "./styles";
 
 export function Home() {
-    return (
-        <View style={styles.container}>
-            <Text>hi!</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Text>hi!</Text>
+			<StatusBar style="auto"/>
+		</View>
+	);
 }
