6c9655af5e268b5543a6a0e4812ec903315b572f
tarcisiowes <tarcisiowesley@gmail.com>
2026-02-25 17:07:24 +0900

    feat: added the button to home + styles

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/Button";
 import { Image, View } from 'react-native';
 import { styles } from "./styles";
 
@@ -5,6 +6,7 @@ export function Home() {
 	return (
 		<View style={styles.container}>
 			<Image source={require('@/assets/logo.png')} style={styles.Logo}/>
+			<Button title={'Add'} onPress={() => console.log("Add")}/>
 		</View>
 	);
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,9 +3,11 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#d0d2d8',
 		alignItems: 'center',
 		justifyContent: 'center',
+		paddingTop: 62,
+		padding: 24,
 	},
 	Logo: {
 		height: 34,
