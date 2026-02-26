85c5bddb9be259582a63e6c20cc692e49b8de45d
tarcisiowes <tarcisiowesley@gmail.com>
2026-02-26 23:24:48 +0900

    chore: created menu screen

---
+++ src/app/Menu/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/Button";
+import { Image, View } from 'react-native';
+import { styles } from "./styles";
+
+export function Home() {
+	return (
+		<View style={styles.container}>
+			<Image source={require('@/assets/logo.png')} style={styles.Logo}/>
+			<Button title={'Save'} onPress={() => console.log("Saved")}/>
+		</View>
+	);
+}
---
+++ src/app/Menu/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#d0d2d8',
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop: 62,
+		padding: 24,
+	},
+	Logo: {
+		height: 34,
+		width: 134,
+	}
+});
