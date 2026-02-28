f5cf0fe5e1da197caf96d6b071c0aae3177f9d26
tarcisiowes <tarcisiowesley@gmail.com>
2026-02-28 22:46:15 +0900

    chore: created filter component

---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#FFFFFF',
+		height: 48,
+		width: '100%',
+		borderRadius: 8,
+		paddingHorizontal: 16,
+		borderWidth: 1,
+	},
+	title: {
+		fontSize: 14,
+		fontWeight: 600,
+		color: '#2c46b1',
+	}
+})
