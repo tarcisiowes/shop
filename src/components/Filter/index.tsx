93243c76850da1a63e3aeec310178aab01fd095b
tarcisiowes <tarcisiowesley@gmail.com>
2026-03-01 21:34:08 +0900

    feat: add filter props

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,6 +1,16 @@
-export function Filter() {
+import { styles } from "@/components/Filter/styles";
+import { FilterStatus } from "@/Types/FilterStatus";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type Props = TouchableOpacityProps & {
+	status: FilterStatus
+	isActive: boolean
+}
+
+export function Filter({status, isActive, ...rest}: Props) {
 	return (
-		<>
-		</>
+		<TouchableOpacity style={styles.container} {...rest}>
+			<Text style={styles.title}>{status === FilterStatus.DONE ? 'Bought' : 'Pending'}</Text>
+		</TouchableOpacity>
 	);
 }
