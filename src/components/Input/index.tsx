5f78f6f245c819802b65563b29c5e8f68a297c4b
tarcisiowes <tarcisiowesley@gmail.com>
2026-03-05 10:18:56 +0900

    styles: added TextInput style adjustments

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,9 @@ import { TextInput, TextInputProps } from "react-native";
 
 export function Input({...rest}: TextInputProps) {
 	return (
-		<TextInput style={styles.container} {...rest} />
+		<TextInput
+			style={styles.container}
+			placeholderTextColor={'#74798b'}
+			{...rest} />
 	);
 }
